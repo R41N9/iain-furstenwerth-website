f1d3b441612d04fb6c0a2d1fff09e35c8abf4478
Iain Furstenwerth <iain.furstenwerth@gmail.com>
2021-11-30 23:33:52 -0600

    built server and index page

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const compression = require("compression");
+const cors = require("cors");
+
+const app = express();
+const port = 80;
+
+app.use(compression);
+app.use(express.static(path.join(__dirname, "../dist")));
+app.use(cors);
+
+app.get("/", (req, res) => {
+  res.send();
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
