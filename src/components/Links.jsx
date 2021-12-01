65b5e6b2f4920af08f8339cf20e187d0b68cd00f
Iain Furstenwerth <iain.furstenwerth@gmail.com>
2021-12-01 05:37:56 -0600

    changed download of resume to just a link. didn't work with any of the instructions on google

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -9,7 +9,7 @@ class Links extends React.Component {
         <h2 id="links-title">Links</h2>
         <a id="github-link" href="https://github.com/R41N9?tab=repositories">My Github</a>
         <a id="linkedin-link" href="https://www.linkedin.com/in/iain-furstenwerth-0a8207209/">My LinkedIn</a>
-        <a id="resume-download" href="https://docs.google.com/document/d/1gUOmNSumhJv9MRmKMjGHBFGsf84hKvVEiN2ufQvkVIs/edit?usp=sharing" download>Download Resume</a>
+        <a id="resume-link" href="https://docs.google.com/document/d/1gUOmNSumhJv9MRmKMjGHBFGsf84hKvVEiN2ufQvkVIs/edit?usp=sharing">My Resume</a>
       </div>
     )
   }
